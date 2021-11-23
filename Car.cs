5b52b13f89139353c215682915fc599780828b09
Shuhad <Shuhad@DESKTOP-LAIUVSI>
2021-11-23 16:34:43 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public double Engine { get; set; }
+        public string Colour { get; set; }
+        public bool BrandNew { get; set; }
+
+
+        public Car()
+        {
+            Make = "Nothing yet";
+            Model = "Not produced yet";
+            Price = 0.00M;
+            Engine = 0.0;
+            Colour = "nothing";
+            BrandNew = true;
+        }
+
+        public Car(string a, string b, decimal c, double d, string e, bool f)
+        {
+            Make = a;
+            Model = b;
+            Price = c;
+            Engine = d;
+            Colour = e;
+            BrandNew = f;
+        }
+
+        public override string ToString()
+        {
+            return Make + " " + Model + " R" + Price + " Engine size: " + Engine + " Colour: " + Colour + " Car is new: " + BrandNew;
+        }
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Store
+    {
+        public List<Car> CarList { get; set; }
+        public List<Car> ShopList { get; set; }
+
+        public Store()
+        {
+            CarList = new List<Car>();
+            ShopList = new List<Car>();
+        }
+
+        public decimal CheckOut()
+        {
+            decimal TotalCost = 0;
+
+            foreach (var item in ShopList)
+            {
+                TotalCost = TotalCost + item.Price;
+            }
+
+            ShopList.Clear();
+            return TotalCost;
+        }
+
+       
+    }
+
+}
